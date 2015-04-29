9a956a945062f761ae26a485758de1fe1de4ba74
yoshitake.nakaji <kokodoko966@gmail.com>
2015-04-30 02:04:08 +0900

    fix sh

--- .travis_build.sh
+++ .travis_build.sh
@@ -3,10 +3,9 @@ echo "Compiling unit tests..."
 #clang -c set.c
 #clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 #clang++ -std=c++11 -g -Wall -Wextra -o unit_test set.o unit_test.o -lgtest -pthread
-g++ -o mytest -lgtest_main -lpthread mytest.c
+g++ hellowa.c mytest.c -lgtest_main -lpthread -o mytest
 echo "Running unit tests..."
 ./mytest
 result=$?
-rm -r mytest.o
 echo "Unit tests completed : $result"
 exit $result
