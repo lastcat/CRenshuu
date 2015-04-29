65d6cff474e246ce7c5eb94cb5fce717fd07c0ba
yoshitake.nakaji <kokodoko966@gmail.com>
2015-04-30 00:55:35 +0900

    add -pthread option

--- .travis_build.sh
+++ .travis_build.sh
@@ -3,7 +3,7 @@ echo "Compiling unit tests..."
 #clang -c set.c
 #clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 #clang++ -std=c++11 -g -Wall -Wextra -o unit_test set.o unit_test.o -lgtest -pthread
-g++ -g -Wall hellowa.c mytest.c -o mytest -lgtest
+g++ -g -Wall -pthread　hellowa.c mytest.c -o mytest -lgtest
 echo "Running unit tests..."
 ./mytest
 result=$?
