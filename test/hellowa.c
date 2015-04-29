09e6d0bc397301c8b3dc7fb54fce838b4a3508d7
yoshitake.nakaji <kokodoko966@gmail.com>
2015-04-30 00:35:41 +0900

    travis setting?

--- test/hellowa.c
+++ test/hellowa.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 GTEST_API_ int main(int argc, char **argv){
   printf("HELLO WORLD\n");
   testing::InitGoogleTest(&argc, argv);
