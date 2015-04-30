874e2cf213f101daffad8f40efdad227f269418e
yoshitake.nakaji <kokodoko966@gmail.com>
2015-04-30 09:34:01 +0900

    fix sh

--- .travis_build.sh
+++ .travis_build.sh
@@ -3,7 +3,8 @@ echo "Compiling unit tests..."
 #clang -c set.c
 #clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 #clang++ -std=c++11 -g -Wall -Wextra -o unit_test set.o unit_test.o -lgtest -pthread
-g++ -o mytest -lgtest_main -lpthread hellowa.c mytest.c
+#g++ -o mytest -lgtest_main -lpthread hellowa.c mytest.c
+g++ -o mytest -lgtest_main -lpthread mytest.c
 echo "Running unit tests..."
 ./mytest
 result=$?
