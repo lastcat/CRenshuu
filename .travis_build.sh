aa65ac16eda31aee83499fd412271d50073f399a
yoshitake.nakaji <kokodoko966@gmail.com>
2015-04-30 09:49:08 +0900

    fix sh

--- .travis_build.sh
+++ .travis_build.sh
@@ -4,7 +4,7 @@ echo "Compiling unit tests..."
 #clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 #clang++ -std=c++11 -g -Wall -Wextra -o unit_test set.o unit_test.o -lgtest -pthread
 #g++ -o mytest -lgtest_main -lpthread hellowa.c mytest.c
-g++ -o mytest -lgtest_main -lpthread mytest.c
+g++ -I$GTEST_INCLUDE_DIRS/include hellowa.c mytest.c $GTEST_LIBRARY $GTEST_MAIN_LIBRARY -o mytest
 echo "Running unit tests..."
 ./mytest
 result=$?
